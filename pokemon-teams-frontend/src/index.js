7ec781f0ac343226244099e33bfe40c80c0ba212
abduamal <abduamal@ymail.com>
2021-05-15 10:52:35 -0500

    frontend

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,15 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadTrainers
+})
+
+const main = () =>  document.querySelector('main')
+
+const loadTrainers = () => {
+  fetch('http://localhost:3000/trainers')
+  .then(response => response.json())
+  .then(json => console.log('Aight Shawty'))
+}
